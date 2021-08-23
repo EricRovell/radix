4b2bde9fd2424167e8f1e191c6f86b57943d4504
Eric Rovell <ericrovell@gmail.com>
2021-08-23 16:03:26 +0300

    create radix transform function

---
+++ src/util/radixTransform.ts
@@ -0,0 +1,31 @@
+/**
+ * Transforms a natural number to it's radix representation.
+ */
+function toDigits(number: number, radix: number): number[] {
+	let source = number;
+	const ranks: number[] = [];
+
+	if (!source) {
+		return [ 0 ];
+	}
+
+	while (source) {
+		ranks.push(source % radix);
+		source = source / radix | 0;
+	}
+	return ranks.slice().reverse();
+}
+
+/**
+ * Computes a number from given digits in given radix
+ */
+function fromDigits(ranks: number[], radix: number) {
+	return ranks.reduce((acc, rank) => radix * acc + rank, 0);
+}
+
+/**
+ * Transforms number's radix.
+ */
+export function radixTransform(input: number[], radixIn: number, radixOut: number): number[] {
+	return toDigits(fromDigits(input, radixIn), radixOut);
+}
